85f8196c25c446146db47843a17983a999e90206
unknown <karitakors@gmail.com>
2026-04-28 15:17:56 +0300

    shuffle

--- js/board.js
+++ js/board.js
@@ -10,7 +10,10 @@ let lockBoard = false;
 let pairs = 0
 
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
 
 export function createBoard() {
